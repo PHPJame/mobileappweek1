4e4f4692a6412bbea6f6633d6f98898aff0d6963
Phuwanet <jame7378@gmail.com>
2021-09-30 16:35:08 +0700

    dashboarod

--- lib/screen/Dashboard.dart
+++ lib/screen/Dashboard.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:mobileappweek1/Modal/TCT.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -8,10 +10,54 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  var data;
+
+  @override
+  void initState() {
+    super.initState();
+    print("Hello");
+    callAPI();
+  }
+
+  Future<void> callAPI() async {
+    var url = Uri.parse("https://www.boredapi.com/api/activity");
+
+    var response = await http.get(url);
+
+    setState(() {
+      data = tctFromJson(response.body);
+    });
+    // print(response.statusCode);
+    //print(response.body);
+    // print(data.activity);
+    // print(data.type);
+    // print(data.price);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Dashboard'),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            children: [
+              Icon(Icons.api),
+              SizedBox(width: 10),
+              Text('Dashboard'),
+            ],
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text(data?.activity ?? "Loading..."),
+              Text(data?.type ?? "Loading..."),
+              Text('${data?.price ?? "Loading..."}'),
+              Text('${data?.participants ?? "Loading..."}'),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
